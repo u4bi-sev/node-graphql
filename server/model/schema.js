a1bd81aac6c51e25c8b3cf13b828f3f1d71cf5ff
u4bi <admin@naver.id>
2018-01-28 13:16:00 +0900

    Added GraphQL mutation

--- server/model/schema.js
+++ server/model/schema.js
@@ -3,15 +3,18 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = require('graphql');
 
-const users = [
+
+var users = [
     { id : '1', name : 'John Gang', email : 'gang@gmail.com', age : 14 },
     { id : '2', name : 'John Kim', email : 'kim@gmail.com', age : 15 },
     { id : '3', name : 'John Min', email : 'min@gmail.com', age : 18 },
     { id : '4', name : 'John Yu', email : 'yu@gmail.com', age : 18 }            
 ];
+
 
 const UserType = new GraphQLObjectType({
     name : 'User',
@@ -52,6 +55,88 @@ const RootQuery = new GraphQLObjectType({
 });
 
 
+const mutation = new GraphQLObjectType({
+    name : 'Mutation',
+    fields : {
+        /* POST
+            /graphql?query=mutation{ 
+                addUser( name : "John Jang", email : "jang@gamil.com", age : 14){
+                    id,
+                    name,
+                    email,
+                    age
+                }
+            }
+        */
+        addUser : {
+            type : UserType,
+            args : {
+                name : { type : new GraphQLNonNull(GraphQLString) },
+                email : { type : new GraphQLNonNull(GraphQLString) },
+                age : { type : new GraphQLNonNull(GraphQLInt) } 
+            },
+            resolve(parentValue, args){
+
+                users.push({ 
+                    id : (users.length+1).toString(), 
+                    name : args.name, 
+                    email : args.email, 
+                    age : args.age 
+                });
+
+                return users[users.length-1];
+            }
+        },
+        /* POST
+            /graphql?query=mutation{
+                removeUser(id : "1"){
+                    id,
+                    name,
+                    email,
+                    age
+                }
+            }
+        */
+        removeUser : {
+            type : UserType,
+            args : {
+                id : { type : new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, args){
+                let user = users.filter(e => e.id === args.id)[0];
+                users = users.filter(e => e.id !== args.id);
+
+                return user;
+            }
+        },
+        /* POST
+            /graphql?query=mutation{
+                updateUser(id : "1" , age : 15 ){
+                    id,
+                    name,
+                    email,
+                    age
+                }
+            }
+        */
+        updateUser : {
+            type : UserType,
+            args : {
+                id : { type : new GraphQLNonNull(GraphQLString)},
+                name : { type : GraphQLString },
+                email : { type : GraphQLString },
+                age : { type : GraphQLInt } 
+            },
+            resolve(parentValue, args){
+                let user = users.filter(e => e.id === args.id)[0];
+                return Object.assign(user, user, { ...args });
+            }
+        },        
+    }
+});
+
+
 module.exports = new GraphQLSchema({
-    query : RootQuery
+    query : RootQuery,
+    mutation
 });
